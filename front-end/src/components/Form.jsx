c40cb9f98ba139e39713ee4e2d6b267fdd6d9179
Antoine <mahassadijoseph@gmail.com>
2025-01-07 11:08:54 +0100

    Initialize project with cleaned base structure

---
+++ front-end/src/components/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>im a form</div>
+    )
+}
+
+export default Form
---
+++ front-end/src/main.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css'
+import App from './App.jsx'
+import Login from './pages/login';
+import Profil from './pages/profil';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/Profil' element={<Login />} />
+      <Route path='/Profil' element={<Profil />} />
+    </Routes>
+  </BrowserRouter>,
+)
